f50a7c361646d5a2f2e23cbfb6114fa4d08e9c17
weerayosong <weerayosong@outlook.com>
2026-05-04 15:26:34 +0700

    fix: re-commit etc3-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import { Fragment } from 'react'
 
 import DTopNav from './components/screens/desktop/DTopNav'
 import DFooter from './components/screens/desktop/DFooter'
 
 function App() {
     return (
-        <Fragment className="min-h-screen font-sans bg-[#f8f6f1] text-[#2d2a26]">
+        <div className="min-h-screen font-sans bg-[#f8f6f1] text-[#2d2a26]">
             <div className="mx-auto flex max-w-281.5 items-center justify-between px-8 py-3">
                 <div>
                     {/* Header จะคงที่อยู่ในทุกหน้า */}
@@ -20,7 +19,7 @@ function App() {
                     <DFooter />
                 </div>
             </div>
-        </Fragment>
+        </div>
     )
 }
 
